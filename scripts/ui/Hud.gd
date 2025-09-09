667061d5764d6090f5a798fdd0ae6ef63ce987db
wasab1kastike <arto.askala@gmail.com>
2025-09-09 19:46:34 +0300

    Add tile selection and resource production

--- scripts/ui/Hud.gd
+++ scripts/ui/Hud.gd
@@ -4,19 +4,32 @@ signal start_pressed
 signal pause_pressed
 
 @onready var resources_label: Label = $ResourcesLabel
+@onready var tile_info_label: Label = $TileInfoLabel
 @onready var start_button: Button = $StartButton
 @onready var pause_button: Button = $PauseButton
 @onready var clock_label: Label = $ClockLabel
 
+
 func _ready() -> void:
-    start_button.pressed.connect(func(): start_pressed.emit())
-    pause_button.pressed.connect(func(): pause_pressed.emit())
+	start_button.pressed.connect(func(): start_pressed.emit())
+	pause_button.pressed.connect(func(): pause_pressed.emit())
+
 
 func update_resources(resources: Dictionary) -> void:
-    var parts: PackedStringArray = []
-    for key in resources.keys().sorted():
-        parts.append("%s: %d" % [key.capitalize(), int(resources[key])])
-    resources_label.text = " ".join(parts)
+	var parts: PackedStringArray = []
+	for key in resources.keys().sorted():
+		parts.append("%s: %d" % [key.capitalize(), int(resources[key])])
+	resources_label.text = " ".join(parts)
+
+
+func update_tile(tile_pos: Vector2i, building: Building) -> void:
+	var text := "Tile: (%d,%d)" % [tile_pos.x, tile_pos.y]
+	if building:
+		text += " - %s" % building.name
+	else:
+		text += " - Empty"
+	tile_info_label.text = text
+
 
 func update_clock(time: float) -> void:
-    clock_label.text = "Time: %.2f" % time
+	clock_label.text = "Time: %.2f" % time
