7ad28c98629cc16fa237e4b4119ebc102434c6ca
wasab1kastike <arto.askala@gmail.com>
2025-09-09 21:37:51 +0300

    Add basic Building tests

---
+++ tests/test_building.gd
@@ -0,0 +1,18 @@
+extends Node
+var Building = preload("res://scripts/core/Building.gd")
+
+func test_upgrade_increments_level(res):
+    var building = Building.new()
+    building.level = 1
+    building.upgrade()
+    if building.level != 2:
+        res.fail("Level after upgrade should be 2")
+
+func test_getters_return_values(res):
+    var building = Building.new()
+    building.construction_cost = {"gold": 100}
+    building.production_rates = {"wood": 10}
+    if building.get_construction_cost().get("gold", 0) != 100:
+        res.fail("Construction cost incorrect")
+    if building.get_production_rates().get("wood", 0) != 10:
+        res.fail("Production rate incorrect")
