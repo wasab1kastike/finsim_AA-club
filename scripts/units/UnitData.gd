68127aa3205d13f79eb00afd33ce84d4171184c4
wasab1kastike <arto.askala@gmail.com>
2025-09-10 13:40:19 +0300

    Add UnitData resource and integrate with unit system

---
+++ scripts/units/UnitData.gd
@@ -0,0 +1,8 @@
+extends Resource
+class_name UnitData
+
+@export var name: String = ""
+@export var max_health: int = 0
+@export var attack: int = 0
+@export var defense: int = 0
+@export var speed: float = 0.0
